f28075fc9649eaf447f2fb231c32f5cbec075a17
Jaap Jan <mail@jaapjan.nl>
2020-11-24 15:08:13 +0100

    Cleanup installation script.

--- scripts/install_site.sh
+++ scripts/install_site.sh
@@ -8,24 +8,8 @@ cd /var/www/html/;
 # Set the correct settings.php requires scripts folder to be mounted in /var/www/scripts/social.
 chmod 777 /var/www/html/sites/default
 
-time drush -y site-install --db-url=mysql://root:root@db:3306/oec --account-pass=admin --site-name='OEC Group dev';
-echo "installed OEC Group dev"
-
-drush -y ucrt jaapjan --password=jaapjan
-drush -y ucrt harry --password=harry
-
-drush sql-dump > /var/www/sql-backups/clean-without-modules.sql
-time drush -y en group
-drush sql-dump > /var/www/sql-backups/clean-with-group.sql
-time drush -y en group_flex
-drush sql-dump > /var/www/sql-backups/clean.sql
-
-#drush cdel field.field.node.article.body
-#drush cdel field.field.node.page.body
-#drush entity:delete shortcut_set
-#drush -y cset system.site uuid "4665d630-8df0-40a5-94c9-0da72da4af0c"
-#drush -y config:import
-#echo "imported configuration"
+time drush -y site-install --db-url=mysql://root:root@db:3306/dds --account-pass=admin --site-name='DDS';
+echo "installed DDS"
 
 php -r 'opcache_reset();';
 echo "opcache reset"
